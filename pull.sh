3632645be43d68fce3f3ed561bf4e0b3bc9647b9
Judson Ward <Judsondward@gmail.com>
2025-10-18 07:39:35 -0500

    Fixed absolute path to relative to home to account for different usernames

--- pull.sh
+++ pull.sh
@@ -1,4 +1,4 @@
-cd /home/eezagi/Games/HorizonXI/Prefix/pfx/drive_c/Program\ Files\ \(x86\)/HorizonXI/Game/SquareEnix/FINAL\ FANTASY\ XI/USER
+cd ~/Games/HorizonXI/Prefix/pfx/drive_c/Program\ Files\ \(x86\)/HorizonXI/Game/SquareEnix/FINAL\ FANTASY\ XI/USER
 git pull
-cd /home/eezagi/Games/HorizonXI/Prefix/pfx/drive_c/Program\ Files\ \(x86\)/HorizonXI/Game/config/addons/luashitacast
+cd ~/Games/HorizonXI/Prefix/pfx/drive_c/Program\ Files\ \(x86\)/HorizonXI/Game/config/addons/luashitacast
 git pull
--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
-cd /home/eezagi/Games/HorizonXI/Prefix/pfx/drive_c/Program\ Files\ \(x86\)/HorizonXI/Game/SquareEnix/FINAL\ FANTASY\ XI/USER
+cd ~/Games/HorizonXI/Prefix/pfx/drive_c/Program\ Files\ \(x86\)/HorizonXI/Game/SquareEnix/FINAL\ FANTASY\ XI/USER
 git add *
 git push
-cd /home/eezagi/Games/HorizonXI/Prefix/pfx/drive_c/Program\ Files\ \(x86\)/HorizonXI/Game/config/addons/luashitacast
+cd ~/Games/HorizonXI/Prefix/pfx/drive_c/Program\ Files\ \(x86\)/HorizonXI/Game/config/addons/luashitacast
 gid add *
 git push

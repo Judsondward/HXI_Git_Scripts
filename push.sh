0c08fa2184a87c3d9de5a14644d32aeea31a2455
Judson Ward <judsondward@gmail.com>
2025-10-18 07:28:01 -0500

    Added Push and Pull scripts

---
+++ push.sh
@@ -0,0 +1,6 @@
+cd /home/eezagi/Games/HorizonXI/Prefix/pfx/drive_c/Program\ Files\ \(x86\)/HorizonXI/Game/SquareEnix/FINAL\ FANTASY\ XI/USER
+git add *
+git push
+cd /home/eezagi/Games/HorizonXI/Prefix/pfx/drive_c/Program\ Files\ \(x86\)/HorizonXI/Game/config/addons/luashitacast
+gid add *
+git push

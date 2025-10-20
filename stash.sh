16e3bcf214b300d9400d50810696b7f17c8d7230
Judson Ward <Judsondward@gmail.com>
2025-10-19 19:05:13 -0500

    more versioning fixes

--- stash.sh
+++
@@ -1,4 +0,0 @@
-cd /home/eezagi/Games/HorizonXI/Prefix/pfx/drive_c/Program\ Files\ \(x86\)/HorizonXI/Game/SquareEnix/FINAL\ FANTASY\ XI/USER
-git stash -u
-cd /home/eezagi/Games/HorizonXI/Prefix/pfx/drive_c/Program\ Files\ \(x86\)/HorizonXI/Game/config/addons/luashitacast
-git stash -u

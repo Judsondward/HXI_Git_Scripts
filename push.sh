c0192a116460727e16ec1e19740066fbd0148fde
Judson Ward <judsondward@gmail.com>
2025-10-19 19:02:59 -0500

    Trying to fix versioning issues

--- push.sh
+++ push.sh
@@ -1,6 +1,8 @@
-cd ~/Games/HorizonXI/Prefix/pfx/drive_c/Program\ Files\ \(x86\)/HorizonXI/Game/SquareEnix/FINAL\ FANTASY\ XI/USER
+cd /home/eezagi/Games/HorizonXI/Prefix/pfx/drive_c/Program\ Files\ \(x86\)/HorizonXI/Game/SquareEnix/FINAL\ FANTASY\ XI/USER
 git add *
+git commit -m "Check in"
 git push
-cd ~/Games/HorizonXI/Prefix/pfx/drive_c/Program\ Files\ \(x86\)/HorizonXI/Game/config/addons/luashitacast
-gid add *
+cd /home/eezagi/Games/HorizonXI/Prefix/pfx/drive_c/Program\ Files\ \(x86\)/HorizonXI/Game/config/addons/luashitacast
+git add *
+git commit -m "Check in"
 git push

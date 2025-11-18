a0f5e13f71419cf245d10358fddf61f2e3d5e698
Judsondward <judsondward@gmail.com>
2025-11-18 11:23:34 -0600

    Check in

---
+++ win_push.sh
@@ -0,0 +1,8 @@
+cd ~/Games/HorizonXI/Game/SquareEnix/FINAL\ FANTASY\ XI/USER
+git add *
+git commit -m "Check in"
+git push
+cd ~/Games/HorizonXI/Game/config/addons/luashitacast
+git add *
+git commit -m "Check in"
+git push
